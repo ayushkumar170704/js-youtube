42be163e3d7fa884f5cd2d3e2da157fafc9d1573
AYUSH KUMAR <155397687+ayushkumar170704@users.noreply.github.com>
2025-04-15 18:30:41 +0000

    mdn pre and postfix

--- jsbasic/03_conversionOperations.js
+++ jsbasic/03_conversionOperations.js
@@ -1,45 +1,85 @@
-let score = undefined
+// let score = undefined
 
 
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
-
-
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof(score));
 
 
-//NaN not a number
-
-// null give 0
-// undefined give  NaN
-// boolean gives you either one or zero
-
-// string gives u nan not a number
-
-// 33 => canbe converted easily into number\
-// 33ab cannot be converted into number and give result NaN its type is still a numner
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-let isLoggedIn = 1
+// //NaN not a number
+
+// // null give 0
+// // undefined give  NaN
+// // boolean gives you either one or zero
+
+// // string gives u nan not a number
+
+// // 33 => canbe converted easily into number\
+// // 33ab cannot be converted into number and give result NaN its type is still a numner
 
 
-let booleanIsLoggedin = Boolean (isLoggedIn)
-
-console.log(booleanIsLoggedin);
+// let isLoggedIn = 1
 
 
-// 1 => true ; 0=> false
-// "" => false
-// "ayush" => true
+// let booleanIsLoggedin = Boolean (isLoggedIn)
+
+// console.log(booleanIsLoggedin);
+
+
+// // 1 => true ; 0=> false
+// // "" => false
+// // "ayush" => true
 
 
 
-let someNumber = 33
-let stringNumebr = String(someNumber)
+// let someNumber = 33
+// let stringNumebr = String(someNumber)
 
-console.log(stringNumebr);
-console.log( typeof stringNumebr);
+// console.log(stringNumebr);
+// console.log( typeof stringNumebr);
+
+
+// operations
+// let value = 3
+
+// let negValue = -value
+
+// console.log(negValue);
+
+
+// basic maths operations console.log(2+2)// (2-2)// (2**2)
+
+
+
+// let str1 = "hello"
+// let str2 = " ayuhs"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1"+ 2);
+// console.log(1 + "2");
+// console.log(1 + "2" + 2);
+
+// console.log(1 + 2 + "2");
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1 , num2 , num3 
+// num1= num2 = num3 = 2+2 
+
+let gameCounter  = 100;
+gameCounter++;
+console.log(gameCounter)
+
+
+// perfix and postfix mdn web doxs
