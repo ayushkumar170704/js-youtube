f31cfe84402222abc53ac3058a07f00508cbd604
AYUSH KUMAR <155397687+ayushkumar170704@users.noreply.github.com>
2025-04-15 17:22:23 +0000

    learnt about datatypes

---
+++ jsbasic/01_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict  "; // treat all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+console.log(3+3) // code readability should be high
+console.log("ayush")
+
+ 
+let name = "histesh"  // variable datatypes
+let age = 20 // number datatype
+let isLoggedIn= false // boolean
+
+
+// number => 2 to pwoer 53
+// bigint
+// string = ""
+// boolean  => true , false
+//null   standalone value // representation of emtpyt value
+// undefined== not  give any value to the variable
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);
+
+
+
+// null is defined as an object 
+// undefied is type of undefined
+
+
---
+++ jsbasic/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail= "ayush@gmail.com"
+var accountPassword = "1234"
+accountCity= "jaipur"
+
+let accountState;
+
+
+
+// accountId = 2 // not allowed
+// perfer not to use var because of issue in bloc scope and funcitonal sscope
+
+accountEmail="hcah.cpm"
+accountPassword="12312341"
+accountCity="renukoot"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId, accountId,accountCity, accountState])
