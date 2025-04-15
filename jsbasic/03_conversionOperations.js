14b697f0eabf4e7940649b867b485a57597b0b38
AYUSH KUMAR <155397687+ayushkumar170704@users.noreply.github.com>
2025-04-15 17:41:34 +0000

    discussed conversionOperations

---
+++ jsbasic/03_conversionOperations.js
@@ -0,0 +1,45 @@
+let score = undefined
+
+
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//NaN not a number
+
+// null give 0
+// undefined give  NaN
+// boolean gives you either one or zero
+
+// string gives u nan not a number
+
+// 33 => canbe converted easily into number\
+// 33ab cannot be converted into number and give result NaN its type is still a numner
+
+
+let isLoggedIn = 1
+
+
+let booleanIsLoggedin = Boolean (isLoggedIn)
+
+console.log(booleanIsLoggedin);
+
+
+// 1 => true ; 0=> false
+// "" => false
+// "ayush" => true
+
+
+
+let someNumber = 33
+let stringNumebr = String(someNumber)
+
+console.log(stringNumebr);
+console.log( typeof stringNumebr);
