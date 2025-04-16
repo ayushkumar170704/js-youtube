8b44d0084e93f646e2c4f79484f200d913cb9622
AYUSH KUMAR <155397687+ayushkumar170704@users.noreply.github.com>
2025-04-16 13:15:38 +0000

    nums and maths

---
+++ jsbasic/06_nums_and_maths.js
@@ -0,0 +1,44 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2)) // used for precision value
+
+
+// const otherNumber = 33.8909
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString("en-In"))   // by default the answer will in american style but after using en-In it will be india numbers system
+
+
+
+// ++++++++++++++++++++++++++++++++++ Maths
+
+// const hundreds = 100000
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+
+console.log((Math.random()* 10 )+ 1) // important method
+
+const min = 10; 
+const max = 20 ;
+console.log(Math.floor(Math.random() * (max - min + 1) + min))   // important note
+
+
+
+
+
+
+
+ 
+
