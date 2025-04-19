9bac5d8de85773d6acc656a7a195c5a70eae484f
AYUSH KUMAR <155397687+ayushkumar170704@users.noreply.github.com>
2025-04-19 18:43:50 +0000

    function part 1

---
+++ 03_jsbasic/01_functions.js
@@ -0,0 +1,17 @@
+
+function sayMyName(){
+    console.log("a");
+    console.log("y");
+    console.log("u");
+    console.log("s");
+    console.log("h");
+
+}
+
+//sayMyName()
+
+function addTwoNumbers(number1, number2){
+     console.log(number1 + number2);
+}
+
+addTwoNumbers(3,4)
