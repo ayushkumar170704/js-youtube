1138c311d63338eebe446b88f4d4c01b01f9f275
AYUSH KUMAR <155397687+ayushkumar170704@users.noreply.github.com>
2025-04-23 19:36:56 +0000

    function 19

--- 03_jsbasic/01_functions.js
+++ 03_jsbasic/01_functions.js
@@ -19,8 +19,21 @@ addTwoNumbers(3,4)
 
 
 
-console.log(addTwoNumbers);
-function divideTwoNumbers(number1 , number2){
-    console.log(number1 / number2);
+// console.log(addTwoNumbers);
+// function divideTwoNumbers(number1 , number2){
+//     console.log(number1 / number2);
+// }
+// divideTwoNumbers(3,4);
+
+function loginUserMessage(username){
+    if(username=== undefined){
+        console.log("please enter a username");
+        return
+    }
+
+    return `${username} just logged in`
 }
-divideTwoNumbers(3,4);
+
+console.log(loginUserMessage())
+
+// if you does not pass any thing as parameter it comes out as undefiend
